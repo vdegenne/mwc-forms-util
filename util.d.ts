b9e9a4a015af91c337324dc5ecae2fac28e5faea
valentin degenne <vdegenne@gmail.com>
2020-01-22 17:48:19 +0100

    v1

---
+++ util.d.ts
@@ -0,0 +1,10 @@
+import { TextArea } from '@material/mwc-textarea';
+export declare function getFormNodes(form: HTMLElement): NodeListOf<HTMLInputElement>;
+export declare function getFormInputs(form: HTMLElement): {
+    [label: string]: HTMLInputElement;
+};
+export declare function validateForm(form: HTMLElement): boolean;
+export declare function serializeForm(form: HTMLElement): any;
+export declare function fillForm(form: HTMLElement, object: any): void;
+export declare function resetForm(form: HTMLElement): Promise<void>;
+export declare function resizeTextArea(textarea: TextArea): void;
